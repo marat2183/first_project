3c33a595e2939f847f60b2b76ecb866a25902c08
marat2183 <marat.grig318@gmail.com>
2020-09-30 00:18:51 +0300

    link_correct

--- projsite/static/js/link_check.js
+++ projsite/static/js/link_check.js
@@ -1,8 +1,10 @@
-$(document).ready(function (event) {
-    $(".menu").click(function () {
-        let link_to_redirect = $(this).children(".title-text").children('a').attr('href')
-        console.log(link_to_redirect);
-        document.location.href = link_to_redirect;
+$(document).ready(function () {
+    $(".menu").click(function (event) {
+        if( event.which == 1 || event.which == 2 ) {
+            let link_to_redirect = $(this).children(".title-text").children('a').attr('href')
+            console.log(link_to_redirect);
+            document.location.href = link_to_redirect;
+        }
     });
     $(".dialog").click(function () {
         let link_to_red = $(this).children(".title-text-dialogs").children('a').attr('href');
